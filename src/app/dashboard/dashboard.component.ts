0c6b09508bed1397731b262bd91fcc5e8385aca6
Kushal Kumar <kushi.17.1987@gmail.com>
2018-07-03 19:50:23 +0530

    Added the life cycle hooks into the Dashboard Component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,
+   OnInit,
+   OnChanges,
+   DoCheck,
+   AfterContentInit,
+   AfterContentChecked,
+   AfterViewInit,
+   AfterViewChecked,
+   OnDestroy,
+   SimpleChanges } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -7,17 +16,47 @@ import { HeroService } from '../hero.service';
   templateUrl: './dashboard.component.html',
   styleUrls: [ './dashboard.component.css' ]
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterViewChecked,
+                                                            AfterContentChecked, AfterViewInit, OnDestroy {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService) {
+      console.log('Constructor Called');
+   }
 
-  ngOnInit() {
-    this.getHeroes();
-  }
+   ngOnInit() {
+      console.log('ngOnInit Called');
+   }
 
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
-  }
+   ngOnChanges(simpleChanges: SimpleChanges) {
+      console.log('ngOnChanges Called', simpleChanges);
+   }
+
+   ngDoCheck() {
+      console.log('ngOnDoCheck Called');
+   }
+
+   ngAfterContentInit() {
+      console.log('ngAfterContentInit Called');
+   }
+
+   ngAfterContentChecked() {
+      console.log('ngAfterContentChecked Called');
+   }
+
+   ngAfterViewInit() {
+      console.log('ngAfterViewInit Called');
+   }
+
+   ngAfterViewChecked() {
+      console.log('ngAfterViewChecked Called');
+   }
+
+   ngOnDestroy() {
+      console.log('ngOnDestroy Called');
+   }
+
+   getHeroes(): void {
+      this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes.slice(1, 5));
+   }
 }
